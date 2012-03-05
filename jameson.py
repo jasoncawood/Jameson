baa32e243ab38c33a13f7c198224393f4cd8980f
Jason Cawood <jasonx.l.cawood@intel.com>
2012-03-05 15:24:57 -0800

    Setup initial variables for the bot.

---
+++ jameson.py
@@ -0,0 +1,7 @@
+# Import some necessary libraries.
+import socket  
+
+# Some basic variables used to configure the bot        
+server = "irc.freenode.net" # Server
+channel = "#youcoolchan" # Channel
+botnick = "Mybot" # Your bots nick
